b6321725ad6655ee2c84c185bb8a42f3990536e4
unknown <you@example.com>
2022-10-13 22:04:20 +0700

    T

--- ss1_Spring_MVC_introduction/exercise/dictionary/src/main/java/com/dictionary/repository/impl/DictionaryRepository.java
+++ ss1_Spring_MVC_introduction/exercise/dictionary/src/main/java/com/dictionary/repository/impl/DictionaryRepository.java
@@ -18,7 +18,15 @@ public class DictionaryRepository implements IDictionaryRepository {
         dictionaries.add(new Dictionary("forever", "mãi mãi"));
         dictionaries.add(new Dictionary("endless", "vĩnh cữu"));
         dictionaries.add(new Dictionary("lovesickness", "tương tư"));
-        dictionaries.add(new Dictionary("depression", "trầm cảm"));
+//        dictionaries.add(new Dictionary("happiness ", "hạnh phúc"));
+//        dictionaries.add(new Dictionary("sadness", "đau khổ"));
+//        dictionaries.add(new Dictionary("confused ", "bối rối"));
+//        dictionaries.add(new Dictionary("hope ", "hy vọng"));
+//        dictionaries.add(new Dictionary("hopeless ", "tuyệt vọng"));
+//        dictionaries.add(new Dictionary("pain ", "đau đớn"));
+//        dictionaries.add(new Dictionary("depressive", "trầm cảm"));
+//        dictionaries.add(new Dictionary("rescue ", "giải thoát"));
+
     }
 
     @Override
