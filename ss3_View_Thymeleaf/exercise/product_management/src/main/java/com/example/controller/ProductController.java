6889594e336a8a92215cf477d343497964c2a2f4
unknown <you@example.com>
2022-10-18 22:11:35 +0700

    T

--- ss3_View_Thymeleaf/exercise/product_management/src/main/java/com/example/controller/ProductController.java
+++ ss3_View_Thymeleaf/exercise/product_management/src/main/java/com/example/controller/ProductController.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import java.util.List;
 
 @Controller
-@RequestMapping("/product")
+//@RequestMapping("/product")
 public class ProductController {
     @Autowired
     private IProductService productService;
@@ -20,53 +20,53 @@ public class ProductController {
     public String showList(Model model) {
         List<Product> productList = productService.findAll();
         model.addAttribute("products", productList);
-        return "/list";
+        return "list";
     }
 
     @GetMapping("/create")
     public String create(Model model) {
         model.addAttribute("product", new Product());
-        return "/create";
+        return "create";
     }
 
     @PostMapping("/save")
     public String save(Product product, RedirectAttributes redirectAttributes) {
         productService.save(product);
-        redirectAttributes.addFlashAttribute("mess", "Add new product successful!");
+        redirectAttributes.addFlashAttribute("mess", "Add new successful!");
         return "redirect:/";
     }
 
-//    @GetMapping("/edit/{id}")
-//    public String edit(@PathVariable int id, Model model) {
-//        model.addAttribute("product", productService.findById(id));
-//        return "/edit";
-//    }
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable int id, Model model) {
+        model.addAttribute("product", productService.findById(id));
+        return "/edit";
+    }
 
     @PostMapping("/update")
     public String update(@ModelAttribute Product product, RedirectAttributes redirectAttributes) {
         productService.update(product);
-        redirectAttributes.addFlashAttribute("mess", "Update product successful!");
+        redirectAttributes.addFlashAttribute("mess", "Update successful!");
         return "redirect:/";
     }
 
-//    @GetMapping("/delete/{id}")
-//    private String delete(@PathVariable int id, Model model) {
-//        model.addAttribute("product", productService.findById(id));
-//        return "/delete";
-//    }
+    @GetMapping("/delete/{id}")
+    private String delete(@PathVariable int id, Model model) {
+        model.addAttribute("product", productService.findById(id));
+        return "/delete";
+    }
 
     @PostMapping("/delete")
     public String delete(Product product, RedirectAttributes redirectAttributes) {
         productService.remove(product.getId());
-        redirectAttributes.addFlashAttribute("mess", "Remove product successful!");
+        redirectAttributes.addFlashAttribute("mess", "Remove successful!");
         return "redirect:/";
     }
 
-//    @GetMapping("/view/{id}")
-//    public String view(@PathVariable int id, Model model) {
-//        model.addAttribute("product", productService.findById(id));
-//        return "/view";
-//    }
+    @GetMapping("/view/{id}")
+    public String view(@PathVariable int id, Model model) {
+        model.addAttribute("product", productService.findById(id));
+        return "/view";
+    }
 
     @GetMapping("/search")
     public String showListSearch(@RequestParam String name, Model model) {
