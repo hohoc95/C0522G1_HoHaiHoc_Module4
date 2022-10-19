4c01ca4a1d41b774cee0b9b40686fa9879256ca8
unknown <you@example.com>
2022-10-19 20:25:10 +0700

    T

---
+++ ss5_ORM_and_JPA/exercise/product_management/src/main/java/com/example/repository/impl/ProductRepository.java
@@ -0,0 +1,46 @@
+package com.example.repository.impl;
+
+import com.example.model.Product;
+import com.example.repository.IProductRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class ProductRepository implements IProductRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<Product> findAll() {
+        TypedQuery<Product> query = entityManager.createQuery("select p from Product as p", Product.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public void save(Product product) {
+        entityManager.persist(product);
+    }
+
+    @Override
+    public void update(Product product) {
+        entityManager.merge(product);
+    }
+
+    @Override
+    public void remove(int id) {
+//        entityManager.remove();
+    }
+
+    @Override
+    public Product findById(int id) {
+
+        return entityManager.find(Product.class, id);
+    }
+
+}
---
+++ ss5_ORM_and_JPA/exercise/product_management/src/main/java/com/example/service/IProductService.java
@@ -0,0 +1,14 @@
+package com.example.service;
+
+import com.example.model.Product;
+
+import java.util.List;
+
+public interface IProductService {
+    List<Product> findAll();
+    void save(Product product);
+    void update(Product product);
+    void remove(int id);
+    Product findById(int id);
+//    List<Product> findByName(String name);
+}
